9a25d6a3c7c7ec06f0da7f96ea2bd6189423fa9e
Witkiewicz, Connor <cwitkiewicz@mtb.com>
2023-07-13 11:35:17 -0400

    Cards built, game deck built, deal cards built, create player built

--- Guild.py
+++ Guild.py
@@ -1,10 +1,22 @@
 import pygame
 import cryptography
+import pygase
+import random
 from PyCode.cards.Alchemist import *
-from PyCode.cards.Alchemist import *
-from PyCode.cards.Alchemist import *
-from PyCode.cards.Alchemist import *
-from PyCode.cards.Alchemist import *
+from PyCode.cards.Assassin import *
+from PyCode.cards.Cleric import *
+from PyCode.cards.Commoner import *
+from PyCode.cards.Jester import *
+from PyCode.cards.King import *
+from PyCode.cards.Knight import *
+from PyCode.cards.Marksman import *
+from PyCode.cards.Merchant import *
+from PyCode.cards.Necromancer import *
+from PyCode.cards.Princess import *
+from PyCode.cards.Queen import *
+from PyCode.cards.Thief import *
+from PyCode.Player import *
+
 
 Tile_Size = 64
 Game_Deck = []
@@ -14,12 +26,41 @@ Players = []
 
 class GuildGame:
     def __init__(self) -> None:
-        
-
-        return True
+        create_cards()  
+        build_deck(4)
     
 def create_cards(): #creates one instance of each card type
     Cards.append(Alchemist())
+    Cards.append(Assassin())
+    Cards.append(Cleric())
+    Cards.append(Commoner())
+    Cards.append(Jester())
+    Cards.append(King())
+    Cards.append(Knight())
+    Cards.append(Marksman())
+    Cards.append(Merchant())
+    Cards.append(Nercomancer())
+    Cards.append(Princess())
+    Cards.append(Queen())
+    Cards.append(Thief())
 
 def build_deck(num_players): #creates the game deck based on number of players
-    pass
+    for card in Cards:
+        Game_Deck.append(card)
+
+def create_players(num_players):
+    for i in range(num_players):
+        Players.append(Player())
+
+def deal_cards():
+    for i in range(3):
+        random.shuffle(Game_Deck)
+    index = 0
+    for i in range(3):
+        for x in Players:
+            x.current_deck.append(Game_Deck[index])
+            index+=1
+        
+GuildGame()
+create_players(3)
+deal_cards()
