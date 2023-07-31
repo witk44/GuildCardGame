b5907b6fefa4add5119d898f722fc8940e805fcf
witk44 <69919362+witk44@users.noreply.github.com>
2023-07-31 10:47:47 -0400

    folders updted

--- code/cards/Commoner.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Commoner:
-    def __init__(self) -> None:
-        self.cost = 0
-        self.cooldown = 0
-        self.ability_description = "While it might seem like the commoner is the most useless card in the game, you would be foolish to think that. When banded together with 2 other commoners a player has the ability to announce a union during their turn. Doing so allows the player to revive one card (if necessary) and all alive commoners are reshuffled for new cards. This means that if you have a dead commoner and two alive, you can get a brand-new deck! If you have two dead commoners though, only one can be revived meaning you only get two new cards. This move cannot be bluffed"
-        
