10b6fe0fc66ae8888baf271c29803ba738bf9544
Witkiewicz, Connor <cwitkiewicz@mtb.com>
2023-08-10 10:50:35 -0400

    king ability

--- resources/PyCode/cards/King.py
+++ resources/PyCode/cards/King.py
@@ -6,3 +6,8 @@ class King:
         self.cost = 0
         self.cooldown = 1
         self.ability_description = "The king allows the player to pocket 3 gold as their turn instead of the usual 1 gold"
+
+    def ability(self,player):
+        player.gold += 3
+        if player.gold >20:
+            player.gold = 20
